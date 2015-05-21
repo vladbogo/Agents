1724afbf4b7790a5d182af6b4caa1fa133cbfa08
vladbogo <vladbogolin@gmail.com>
2015-05-21 21:32:25 +0200

    count the steps

--- src/Constants.java
+++ src/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	// General constants.
 	public static final int CANVAS_SIZE = 600;
 	public static final String TITLE = "Agents";
-	public static final int SLEEP = 600;
+	public static final int SLEEP = 1;
 	public static final Color NOTVISIBLE_COLOR = Color.BLACK;
 
 	// World colors
